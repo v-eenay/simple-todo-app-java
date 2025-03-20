47f2842f99d4d1eebb5cf38063104ae678c1c356
v-eenay <koiralavinay@gmail.com>
2025-03-20 11:32:14 +0545

    Sample commit for new branch

---
+++ src/main/java/controller/UserAuthenticationServlet.java
@@ -0,0 +1,18 @@
+package controller;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UserAuthenticationServlet", value = "/UserAuthenticationServlet")
+public class UserAuthenticationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
